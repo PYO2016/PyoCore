cf3ccf4c28b6c60a125687339b517f8a27be89b8
Taeguk Kwon <xornrbboy@gmail.com>
2015-11-25 01:57:43 +0900

    add DLL import example in C++.

---
+++ examples/Cpp-Import/main.cpp
@@ -0,0 +1,24 @@
+#include "../../PyoCore/PyoCore.h"
+
+#include <cstdio>
+
+int main(void)
+{
+	printf("C++ import DLL examples\n\n");
+
+	PyoCore::helloWorld();
+	printf("\n");
+
+	CHAR str1[10000];
+	PyoCore::processImageFileA("ANSI-test", PyoCore::IMAGE_FILE_TYPE_PNG, str1);
+	printf("%s\n\n", str1);
+
+	WCHAR str2[10000];
+	PyoCore::processImageFileW(L"WIDE-test", PyoCore::IMAGE_FILE_TYPE_PNG, str2);
+	wprintf(L"%s\n\n", str2);
+
+	int errorCode = PyoCore::getErrorCode();
+	printf("ErrorCode : %d\n", errorCode);
+
+	return 0;
+}
