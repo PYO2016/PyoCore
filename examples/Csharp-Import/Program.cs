92409b94a2c74ffa3d0dfabb6c7c46398b4535fb
Taeguk Kwon <xornrbboy@gmail.com>
2015-11-25 01:58:47 +0900

    add DLL import example in C#

---
+++ examples/Csharp-Import/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PyoCore;
+
+namespace Csharp_Import
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# import DLL examples\n");
+            PyoCore.PyoCore.helloWorld();
+            Console.WriteLine();
+
+            StringBuilder str1 = new StringBuilder();
+            PyoCore.PyoCore.processImageFileA("ANSI-test", PyoCore.ImageFileType.IMAGE_FILE_TYPE_PNG, str1);
+            Console.WriteLine(str1 + "\n");
+
+            StringBuilder str2 = new StringBuilder();
+            PyoCore.PyoCore.processImageFileW("WIDE-test", PyoCore.ImageFileType.IMAGE_FILE_TYPE_PNG, str2);
+            Console.WriteLine(str2 + "\n");
+
+            int errorCode = PyoCore.PyoCore.getErrorCode();
+            Console.WriteLine("ErrorCode : " + errorCode);
+        }
+    }
+}
---
+++ examples/Csharp-Import/PyoCore.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;     // DLL support
+
+namespace PyoCore
+{
+    class PyoCore
+    {
+        [DllImport("PyoCore.dll")]
+        public static extern void helloWorld();
+
+        [DllImport("PyoCore.dll")]
+        public static extern int processImageFileW (
+            [param: MarshalAs(UnmanagedType.LPWStr)]
+            String imageFileName,
+            [param: MarshalAs(UnmanagedType.I4)]
+            ImageFileType imageFileType,
+            [param: MarshalAs(UnmanagedType.LPWStr)]
+            StringBuilder outputFileName
+            );
+        
+        [DllImport("PyoCore.dll")]
+        public static extern int processImageFileA (
+            [param: MarshalAs(UnmanagedType.LPStr)]
+            String imageFileName,
+            [param: MarshalAs(UnmanagedType.I4)]
+            ImageFileType imageFileType,
+            [param: MarshalAs(UnmanagedType.LPStr)]
+            StringBuilder outputFileName
+            );
+
+        [DllImport("PyoCore.dll")]
+        public static extern int getErrorCode();
+    }
+
+    enum ImageFileType
+    {
+        IMAGE_FILE_TYPE_PNG,
+
+        /* Not image file type. Just for the number of image file types */
+        IMAGE_FILE_TYPE_CNT
+    };
+
+    enum ErrorCode
+    {
+        /* No error */
+        ERROR_NONE,
+        /* Unknown error */
+        ERROR_UNKNOWN,
+        /* Not supported image file type */
+        ERROR_IMAGE_FILE_TYPE,
+
+        /* Not error code. Just for the number of error codes */
+        ERROR_CODE_CNT
+    }
+}
