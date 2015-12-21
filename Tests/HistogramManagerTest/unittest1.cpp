8921d1a98a2aec5a93caaf43d2d1e5a5ffadcf85
Taeguk Kwon <xornrbboy@gmail.com>
2015-12-21 15:45:03 +0900

    add unit test project of HistogramManager

---
+++ Tests/HistogramManagerTest/unittest1.cpp
@@ -0,0 +1,29 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "../../PyoCore/TableDetection/HistogramManager.h"
+#include "../../PyoCore/Common/PngImage.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace HistogramManagerTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		/* Tests for Histogram */
+
+		TEST_METHOD(TestCalculateValues)
+		{
+			// TODO: 테스트 코드를 여기에 입력합니다.
+		}
+
+		TEST_METHOD(TestApplyMedianFilter)
+		{
+			// TODO: 테스트 코드를 여기에 입력합니다.
+		}
+
+		/* Tests for HistogramManager */
+
+	};
+}
