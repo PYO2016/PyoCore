9352d87fe78497c8bda95b3283f02b7b97a28f26
Taeguk Kwon <xornrbboy@gmail.com>
2015-12-23 16:10:58 +0900

    add PngImageTest project. (just add)

---
+++ Tests/PngImageTest/unittest1.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace PngImageTest
+{		
+	TEST_CLASS(HistogramManagerTest)
+	{
+	public:
+		
+		TEST_METHOD(TestLoadAndStore)
+		{
+			// TODO: 테스트 코드를 여기에 입력합니다.
+		}
+
+	};
+}
